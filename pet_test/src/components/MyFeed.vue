a7a38084098a26c91b2c6973e09a91f6e9d09483
AST09\ast09 <osihyeon1124@gmail.com>
2024-03-20 05:08:07 +0900

    펫 엑티비티 페이지 구현 중

--- pet_test/src/components/MyFeed.vue
+++ pet_test/src/components/MyFeed.vue
@@ -76,52 +76,6 @@
 				</div>
 			</div>
 		</section>
-		
-		<!-- Footer -->
-		<footer id="footer">
-			<div class="container">
-				<div class="row double">
-					<div class="6u">
-						<div class="row collapse-at-2">
-							<div class="6u">
-								<h3>Contributors</h3>
-								<ul class="alt">
-									<li><a href="#">박세한 : </a></li>
-									<li><a href="#">김미성 : </a></li>
-									<li><a href="#">박현오 : </a></li>
-									<li><a href="#">오시현 : </a></li>
-									<li><a href="#">주나영 : </a></li>
-								</ul>
-							</div>
-							<div class="6u">
-								<h3>Contact</h3>
-								<ul class="alt">
-									<li><a href="#">kkuldong12@gmail.com</a></li>
-									<li><a href="#">abcde1234@gmail.com</a></li>
-									<li><a href="#">efgh5678@gmail.com</a></li>
-									<li><a href="#">lmno9012@gmail.com</a></li>
-									<li><a href="#">pqrs3456@gmail.com</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class="6u">
-						<h2>백엔드 개발자 프로젝트</h2>
-							<p>상기 서비스는 프로젝트 발표 및 포트폴리오를 목표로 하며 상업적인 용도로 이용될 소지가 없습니다. 또한 가져온 정보들은 모두 무료 오픈소스들을 이용하였습니다.</p>
-								<ul class="icons">
-									<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-									<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
-								</ul>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<div class="copyright">
-			Made by: <a href="#">Kitri</a>
-		</div>
 	</body>	 
 </template>
 
