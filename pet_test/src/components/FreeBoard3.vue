a3998441c83c234798d0b09587d6ad286de496c5
DESKTOP-V0IM9A7\kitri <na9173@suwon.ac.kr>
2024-03-27 09:46:47 +0900

    - freboard, navbar2 오류 수정

--- pet_test/src/components/FreeBoard3.vue
+++ pet_test/src/components/FreeBoard3.vue
@@ -324,7 +324,6 @@ export default {
       this.fetchPostData(postId);
     }
   }
-  }
 }
 </script>
 
--- pet_test/src/components/NavBar2.vue
+++ pet_test/src/components/NavBar2.vue
@@ -1,6 +1,6 @@
 <template>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
- 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ 	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
 	    	<a class="navbar-brand" href="/"><span class="flaticon-pawprint-1 mr-2"></span>Pet sitting</a>
